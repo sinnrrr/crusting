2f826329b3c6d3acb7cec86fc508ab26185b35d5
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-08-01 23:56:50 +0200

    init

---
+++ fearless_concurrency/src/main.rs
@@ -0,0 +1,117 @@
+use std::{
+    sync::{mpsc, Mutex},
+    thread,
+    time::Duration,
+    vec::Vec,
+};
+
+fn hello_threads() {
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+
+    thread::spawn(move || vec.push(3));
+    // vec.push(4); // non posibilita!
+}
+
+fn wait_for_thread() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
+
+fn hello_message_passing() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // println!("val is {}", val); // nah, val is borrowed
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+fn message_passing_works() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn mpsc_in_action() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+fn hello_mutex() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
+
+fn main() {
+    hello_threads();
+    wait_for_thread();
+    hello_message_passing();
+    message_passing_works();
+    mpsc_in_action();
+    hello_mutex();
+}
